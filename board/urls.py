fab78150632961740d1cb4f2a0714478b052cbee
A540 <51090700+A540@users.noreply.github.com>
2024-02-27 16:17:14 +0900

    Feat CRUD complete

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ReadboardView, CreateboardView, UploadboardView
+from .views import *
 
 urlpatterns = [
     path('readboard/', ReadboardView.as_view(), name='ReadboardView'),
     path('postboard/', CreateboardView.as_view(), name='PostboardView'),
-    path('uploadboard', UploadboardView.as_view(), name='PostboardView')
+    path('uploadboard', UploadboardView.as_view(), name='UploadboardView'),
+    path('selectboard/<int:pk>', SelectboardView.as_view(), name='selectboardView'),
+    path('deleteboard/<int:pk>', DeleteboardView.as_view(), name='deleteboardView'),
 ]
--- board/views.py
+++ board/views.py
@@ -28,3 +28,19 @@ class UploadboardView(APIView):
             return Response(status=200)
         else:
             return Response(status=204)
+
+class SelectboardView(APIView):
+    def get(self, request, pk):
+        p_board = board.objects.get(id=pk)
+        p_board.content = p_board.content.replace('\r\n', '<br>')
+
+        return render(request, 'board/selectboard.html', context={'p_board': p_board})
+
+class DeleteboardView(APIView):
+    def post(self, request, pk):
+        try:
+            d_board = board.objects.get(id=pk)
+            d_board.delete()
+            return Response(status=200)
+        except board.DoesNotExist:
+            return Response(status=403)
